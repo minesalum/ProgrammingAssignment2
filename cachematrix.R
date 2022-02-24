1a872a6627ac2d10f10fc845055d01a3386320e9
minesalum <heather.m.melrose@gmail.com>
2022-02-23 18:25:50 -0700

    Getting ready to push cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions cache and compute an inverse matrix where
+## the first function gets and sets the value of a matrix and it's inverse, 
+## and the second function sets the inverse function from the first return.
 
-## Write a short comment describing this function
+## makeCacheMatrix takes one arguement, of the class matrix, and uses 
+## lexical scoping lapply and a super-assignment operator so that
+## it may be used in the second function, aka a different environment from 
+## the one in which it was created.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <<- NULL
+  set <- function(y){
+    x <<- y
+    i <<-NULL
+  }
+  get <- function() x
+  getInverse <<- lapply(x, function(solve) solve(x)) 
+  list(get=get, getInverse=getInverse[1])
 }
 
-
-## Write a short comment describing this function
+## cacheSolve retrieves an inverse from cache, if it exists, using a 
+## variable defined in the first function. If not,
+## then it computes the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  j <- getInverse[1]
+  if(!is.null(j)){
+    message("Getting cached data, hold please.")
+    return(j)
+  }
+  data <- x$get()
+  j <- solve(data, ...)
+  x$getInverse(j)
+  j
 }
